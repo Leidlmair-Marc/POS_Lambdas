d2c2dca84a85f88f3d11963455c554bf3525c888
Marc <leidlmairM21047@sus.htl-grieskirchen.at>
2024-09-24 11:23:08 +0200

    2. Task done

--- POS_Lambdas/src/htl/grieskirchen/POS/HalloJavaMitForEach.java
+++ POS_Lambdas/src/htl/grieskirchen/POS/HalloJavaMitForEach.java
@@ -1,8 +1,18 @@
 package htl.grieskirchen.POS;
 
+import java.util.ArrayList;
+
 public class HalloJavaMitForEach {
 
     public static void main(String[] args) {
-        
+        ArrayList<String> list = new ArrayList<>();
+
+        for(int i = 0; i< list.size();i++){
+            System.out.println(list.get(i));
+        }
+
+        list.forEach(item -> System.out.println(item));
+
+        list.forEach(System.out::println);
     }
 }
