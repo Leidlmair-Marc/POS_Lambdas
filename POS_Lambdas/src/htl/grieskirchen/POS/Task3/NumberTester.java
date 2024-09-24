e3185a5ed564bd7be910174584779b30facd06a9
Marc <leidlmairM21047@sus.htl-grieskirchen.at>
2024-09-24 12:17:55 +0200

    test - and readFile coded

--- POS_Lambdas/src/htl/grieskirchen/POS/Task3/NumberTester.java
+++ POS_Lambdas/src/htl/grieskirchen/POS/Task3/NumberTester.java
@@ -1,5 +1,9 @@
 package htl.grieskirchen.POS.Task3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class NumberTester{
 
     private NumberTest oddTester;
@@ -17,5 +21,40 @@ public class NumberTester{
     }
     public void setPalindromeTester(NumberTest palindromeTester){
         this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile(){
+        for (int[] i : readFile()) {
+            switch (i[0]) {
+                case 1:
+                    System.out.println(oddTester.testNumber(i[1]) ? "EVEN" : "ODD");
+                    break;
+                case 2:
+                    System.out.println(primeTester.testNumber(i[1]) ? "PRIME" : "NOT PRIME");
+                    break;
+                case 3:
+                    System.out.println(palindromeTester.testNumber(i[1]) ? "PALINDROME" : "NOT PALINDROME");
+                    break;
+            }
+        }
+    }
+
+    public int[][] readFile() {
+        int[][] testCases = null;
+        BufferedReader in;
+        try {
+            in = new BufferedReader(new FileReader(fileName));
+            testCases = new int[Integer.parseInt(in.readLine())][2];
+
+            for (int i = 0; in.ready(); i++) {
+                String[] line = in.readLine().split(" ");
+                testCases[i][0] = Integer.parseInt(line[0]);
+                testCases[i][1] = Integer.parseInt(line[1]);
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return testCases;
     }
 }
